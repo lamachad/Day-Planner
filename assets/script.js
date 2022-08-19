10465dbc0ea0e96cee964a1ecbf280b98ded4346
Leandro <machadoleandro98@gmail.com>
2022-08-19 15:43:25 -0700

    declare all variables

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,9 @@
-
+var time9 = parseInt($("#9").attr("data-value"));
+var time10 = parseInt($("#10").attr("data-value"));
+var time11 = parseInt($("#11").attr("data-value"));
+var time12 = parseInt($("#12").attr("data-value"));
+var time13 = parseInt($("#13").attr("data-value"));
+var time14 = parseInt($("#14").attr("data-value"));
+var time15 = parseInt($("#15").attr("data-value"));
+var time16 = parseInt($("#16").attr("data-value"));
+var time17 = parseInt($("#17").attr("data-value"));
