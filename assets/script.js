ebda1ca79bc450bf850bb56f7af131394b99e2d5
Leandro <machadoleandro98@gmail.com>
2022-08-19 15:45:58 -0700

    save button function

--- assets/script.js
+++ assets/script.js
@@ -19,3 +19,92 @@ $(".saveBtn").on("click", function () {
     localStorage.setItem("time16", $("#input16").val());
     localStorage.setItem("time17", $("#input17").val());
   });
+  function color() {
+    var currentTime = moment().hour();
+  
+    if (time9 > currentTime) {
+      $(".acts9").addClass("future");
+    } else if (time9 === currentTime) {
+      $(".acts9").addClass("present");
+    } else {
+      $(".acts9").addClass("past");
+    }
+  
+    if (time10 > currentTime) {
+      $(".acts10").addClass("future");
+    } else if (time10 === currentTime) {
+      $(".acts10").addClass("present");
+    } else {
+      $(".acts10").addClass("past");
+    }
+  
+    if (time11 > currentTime) {
+      $(".acts11").addClass("future");
+    } else if (time11 === currentTime) {
+      $(".acts11").addClass("present");
+    } else {
+      $(".acts11").addClass("past");
+    }
+  
+    if (time12 > currentTime) {
+      $(".acts12").addClass("future");
+    } else if (time12 === currentTime) {
+      $(".acts12").addClass("present");
+    } else {
+      $(".acts12").addClass("past");
+    }
+  
+    if (time13 > currentTime) {
+      $(".acts13").addClass("future");
+    } else if (time13 === currentTime) {
+      $(".acts13").addClass("present");
+    } else {
+      $(".acts13").addClass("past");
+    }
+  
+    if (time14 > currentTime) {
+      $(".acts14").addClass("future");
+    } else if (time14 === currentTime) {
+      $(".acts14").addClass("present");
+    } else {
+      $(".acts14").addClass("past");
+    }
+  
+    if (time15 > currentTime) {
+      $(".acts15").addClass("future");
+    } else if (time15 === currentTime) {
+      $(".acts15").addClass("present");
+    } else {
+      $(".acts15").addClass("past");
+    }
+  
+    if (time16 > currentTime) {
+      $(".acts16").addClass("future");
+    } else if (time16 === currentTime) {
+      $(".acts16").addClass("present");
+    } else {
+      $(".acts16").addClass("past");
+    }
+  
+    if (time17 > currentTime) {
+      $(".acts17").addClass("future");
+    } else if (time17 === currentTime) {
+      $(".acts17").addClass("present");
+    } else {
+      $(".acts17").addClass("past");
+    }
+  }
+  
+  color(); 
+  
+  
+  $("#input9").val(localStorage.getItem("time9"));
+  $("#input10").val(localStorage.getItem("time10"));
+  $("#input11").val(localStorage.getItem("time11"));
+  $("#input12").val(localStorage.getItem("time12"));
+  $("#input13").val(localStorage.getItem("time13"));
+  $("#input14").val(localStorage.getItem("time14"));
+  $("#input15").val(localStorage.getItem("time15"));
+  $("#input16").val(localStorage.getItem("time16"));
+  $("#input17").val(localStorage.getItem("time17"));
+  
